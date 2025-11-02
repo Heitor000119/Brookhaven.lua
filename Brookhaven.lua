8b1c8298d2ddfa83aa0739788c439015ade8fd01
Heitor000119 <h03983547@gmail.com>
2025-11-02 08:05:15 -0300

    Update minimize button image in Brookhaven.lua

--- Brookhaven.lua
+++ Brookhaven.lua
@@ -11,7 +11,7 @@ local Window = Libary:MakeWindow({
     Flags = "KultistHub_Broookhaven"
 })
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://948028207", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://83441255819217", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
